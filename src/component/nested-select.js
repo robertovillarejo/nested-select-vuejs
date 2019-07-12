7cd4e5ee5a5b4e19e10b838ff1a3d27e24123cf2
Roberto Villarejo Martínez <robertovillarejo@outlook.com>
2019-07-12 16:18:31 -0500

    Show 0 depth select

--- src/component/nested-select.js
+++ src/component/nested-select.js
@@ -1,19 +1,66 @@
+import axios from '../../node_modules/axios/dist/axios';
+
 export default {
   name: 'nested-select',
   components: {},
-  props: [],
-  data () {
+  props: {
+    host: {
+      type: String,
+      default: ''
+    },
+    context: {
+      type: String,
+      default: '/api'
+    },
+    enable: {
+      type: Boolean,
+      default: true
+    },
+    hierarchymodel: {
+      type: Array,
+      required: true
+    }
+  },
+  data() {
     return {
-
+      selects: []
     }
   },
   computed: {
 
   },
-  mounted () {
-
+  created() {
+  },
+  mounted() {
+    this.getChildren(0);
   },
   methods: {
-
+    getChildren: function (depth, selected) {
+      var vi = this;
+      var currentNode = vi.hierarchymodel[depth];
+      if (currentNode.path) {
+        const fixedPath = this.fixUrl(currentNode.path, selected);
+        const url = vi.host + vi.context + fixedPath;
+        axios.get(url).then(function (res) {
+          if (vi.selects.length == 0) {
+            vi.selects = new Array(1);
+          }
+          vi.selects[depth] = {
+            options: res.data || new Array(res),
+            prop: currentNode.prop,
+            label: currentNode.label
+          };
+        });
+      }
+    },
+    selectChanged: function (value, selectIndex) {
+      if (selectIndex !== this.hierarchymodel.length - 1) {
+        //this.selects = this.selects.slice(0, selectIndex + 1);
+        this.getChildren(selectIndex + 1, value);
+      }
+    },
+    fixUrl: function (path, selected) {
+      return path.replace('{selected}', selected);
+    }
   }
 }
